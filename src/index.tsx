0b5b52b1c78bbb82f2cf34134e59ed0122a2d747
Petr Hurtak <petr.hurtak@gmail.com>
2023-01-06 17:27:03 +0100

    add strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import "normalize.css";
 import "sakura.css";
 
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import { App } from "./app/app";
 
 const rootEl = document.querySelector("#root");
 if (rootEl) {
-  createRoot(rootEl).render(<App />);
+  createRoot(rootEl).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
 } else {
   throw new Error("Missing app root element");
 }
